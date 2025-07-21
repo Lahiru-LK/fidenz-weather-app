cc1a5e8372986df2fc12b3751563b1f0af1c37b7
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2025-07-21 06:06:07 +0530

    remove console logs

--- backend/src/config/openWeatherConfig.js
+++ backend/src/config/openWeatherConfig.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("openWeatherConfig.js: process.env.OPENWEATHER_API_KEY =", process.env.OPENWEATHER_API_KEY);
 export const openWeatherConfig = {
   apiKey: process.env.OPENWEATHER_API_KEY || "",
 };
